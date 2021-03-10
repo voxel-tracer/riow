d2e0ceab4fc96305228f13ad438272bcffd4409a
VoxelTracer <voxel.tracer@gmail.com>
2021-03-10 06:42:21 -0800

    A simple diffuse material

--- riow/riow.cpp
+++ riow/riow.cpp
@@ -12,11 +12,17 @@
 
 using namespace std;
 
-color ray_color(const ray& r, const hittable_list& world) {
+color ray_color(const ray& r, const hittable_list& world, int depth) {
+    // If we've exceeded the ray bounce limit, no more lights gathered
+    if (depth == 0)
+        return color{ 0, 0, 0 };
+
     hit_record rec;
     if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray{ rec.p, target - rec.p }, world, depth - 1);
     }
+
     vec3 unit_direction = r.direction();
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color { 1.0, 1.0, 1.0 } + t * color{ 0.5, 0.7, 1.0 };
@@ -30,6 +36,7 @@ int main()
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     // World
 
@@ -52,7 +59,7 @@ int main()
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
 
             write_color(cout, pixel_color, samples_per_pixel);
--- riow/vec3.h
+++ riow/vec3.h
@@ -45,6 +45,14 @@ public:
         return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
     }
 
+    inline static vec3 random() {
+        return vec3{ random_double(), random_double(), random_double() };
+    }
+
+    inline static vec3 random(double min, double max) {
+        return vec3{ random_double(min, max), random_double(min, max), random_double(min, max) };
+    }
+
 public:
     double e[3];
 };
@@ -98,4 +106,12 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
 }
