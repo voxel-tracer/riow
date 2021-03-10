49e83dd7c1ac8ee2339fe91631a8a2cda8a4bbd1
VoxelTracer <voxel.tracer@gmail.com>
2021-03-09 20:38:20 -0800

    Common Constants and Utility Functions

--- riow/riow.cpp
+++ riow/riow.cpp
@@ -1,46 +1,42 @@
 // riow.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "rtweekend.h"
 
-#include "vec3.h"
 #include "color.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
+
+#include <iostream>
 
 using namespace std;
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    // we omit a as r.direction() is normalized
-    auto half_b = dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = half_b * half_b - c;
-    if (discriminant < 0)
-        return -1.0;
-    else
-        return -(half_b + sqrt(discriminant));
-}
-
-color ray_color(const ray& r) {
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable_list& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
-    
     vec3 unit_direction = r.direction();
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color { 1.0, 1.0, 1.0 } + t * color{ 0.5, 0.7, 1.0 };
 }
 
 int main()
 {
     // Image
+
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
 
+    // World
+
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
+
     // Camera
+
     auto viewport_height = 2.0;
     auto viewport_width = aspect_ratio * viewport_height;
     auto focal_length = 1.0;
@@ -60,8 +56,10 @@ int main()
             auto u = double(i) / (image_width - 1);
             auto v = double(j) / (image_height - 1);
             ray r{ origin, lower_left_corner + u * horizontal + v * vertical - origin };
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(cout, pixel_color);
         }
     }
+
+    cerr << "\nDone.\n";
 }
