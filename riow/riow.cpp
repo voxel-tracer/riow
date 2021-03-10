9a4512030bb6afa37eac47faf499aaf9b31ae3fc
VoxelTracer <voxel.tracer@gmail.com>
2021-03-09 16:35:16 -0800

    Adding a sphere

--- riow/riow.cpp
+++ riow/riow.cpp
@@ -9,7 +9,20 @@
 
 using namespace std;
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = r.origin() - center;
+    // a == 1 because r.direction is always normalized
+    auto a = 1.0; // dot(r.direction(), r.direction());
+    auto b = 2.0 * dot(oc, r.direction());
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color{ 1, 0, 0 };
+
     vec3 unit_direction = r.direction();
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color { 1.0, 1.0, 1.0 } + t * color{ 0.5, 0.7, 1.0 };
