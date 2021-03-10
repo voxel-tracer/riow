0298aa50c55b9fa5912f11aecea857200f937c59
VoxelTracer <voxel.tracer@gmail.com>
2021-03-09 17:11:52 -0800

    Simplifying the ray-sphere intersection code

--- riow/riow.cpp
+++ riow/riow.cpp
@@ -11,15 +11,14 @@ using namespace std;
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    // a == 1 because r.direction is always normalized
-    auto a = 1.0; // dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
+    // we omit a as r.direction() is normalized
+    auto half_b = dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto discriminant = half_b * half_b - c;
     if (discriminant < 0)
         return -1.0;
     else
-        return (-b - sqrt(discriminant)) / (2.0 * a);
+        return -(half_b + sqrt(discriminant));
 }
 
 color ray_color(const ray& r) {
