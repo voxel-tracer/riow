a5e7e7350a4b3b03cbd4fe62b15435b3d9f320b8
VoxelTracer <voxel.tracer@gmail.com>
2021-03-10 18:16:46 -0800

    An alternative Diffuse formulation

--- riow/riow.cpp
+++ riow/riow.cpp
@@ -19,7 +19,7 @@ color ray_color(const ray& r, const hittable_list& world, int depth) {
 
     hit_record rec;
     if (world.hit(r, 0.001, infinity, rec)) {
-        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        point3 target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
         return 0.5 * ray_color(ray{ rec.p, target - rec.p }, world, depth - 1);
     }
 
