f019842cd42a32dac2646375cf5cb6ac978281db
VoxelTracer <voxel.tracer@gmail.com>
2021-03-11 06:58:01 -0800

    Fuzzy reflection

--- riow/material.h
+++ riow/material.h
@@ -31,14 +31,15 @@ public:
 
 class metal : public material {
 public:
-    metal(const color& a) : albedo(a) {}
+    metal(const color& a, double f = 0.0) : albedo(a), fuzz(f) {}
 
     virtual bool scatter(const ray& in, const hit_record& rec, color& attenuation, ray& scattered) const override {
         vec3 reflected = reflect(in.direction(), rec.normal);
-        scattered = ray{ rec.p, reflected };
+        scattered = ray{ rec.p, reflected + fuzz * random_in_unit_sphere() };
         attenuation = albedo;
         return (dot(reflected, rec.normal) > 0);
     }
 
     color albedo;
+    double fuzz;
 };
--- riow/riow.cpp
+++ riow/riow.cpp
@@ -39,7 +39,7 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 1024;
     const int max_depth = 50;
 
     // World
@@ -48,8 +48,8 @@ int main()
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
