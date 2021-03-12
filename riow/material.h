84e04c0224b15afc9098560fb8b7d3bb97651393
VoxelTracer <voxel.tracer@gmail.com>
2021-03-12 07:59:55 -0800

    Dielectric material

--- riow/material.h
+++ riow/material.h
@@ -28,7 +28,6 @@ public:
     color albedo;
 };
 
-
 class metal : public material {
 public:
     metal(const color& a, double f = 0.0) : albedo(a), fuzz(f) {}
@@ -42,4 +41,38 @@ public:
 
     color albedo;
     double fuzz;
+};
+
+class dielectric: public material {
+public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    virtual bool scatter(const ray& in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        attenuation = color{ 1.0, 1.0, 1.0 };
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        double cos_theta = fmin(dot(-in.direction(), rec.normal), 1.0);
+        double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+        bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+        vec3 direction;
+
+        if (cannot_refract || reflectance(cos_theta, ir) > random_double())
+            direction = reflect(in.direction(), rec.normal);
+        else
+            direction = refract(in.direction(), rec.normal, refraction_ratio);
+
+        scattered = ray{ rec.p, direction };
+        return true;
+    }
+
+    double ir; // Index of Refraction
+
+private:
+    static double reflectance(double cosine, double ref_idx) {
+        // Use Schlick's approximation for reflectance
+        auto r0 = (1 - ref_idx) / (1 + ref_idx);
+        r0 = r0 * r0;
+        return r0 + (1 - r0) * pow((1 - cosine), 5);
+    }
 };
--- riow/riow.cpp
+++ riow/riow.cpp
@@ -37,7 +37,7 @@ int main()
     // Image
 
     const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
+    const int image_width = 800;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int samples_per_pixel = 1024;
     const int max_depth = 50;
@@ -47,13 +47,14 @@ int main()
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    auto material_left = make_shared<dielectric>(1.5);
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
