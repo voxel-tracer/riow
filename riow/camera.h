1eee30bb7f25e4f2a79701e4ce790c60bdd0c082
VoxelTracer <voxel.tracer@gmail.com>
2021-03-12 17:44:55 -0800

    Positioning and Orienting the Camera

--- riow/camera.h
+++ riow/camera.h
@@ -4,20 +4,29 @@
 
 class camera {
 public:
-    camera() {
-        const auto aspect_ratio = 16.0 / 9.0;
-        auto viewport_height = 2.0;
+    camera(
+        point3 look_from,
+        point3 look_at,
+        vec3 vup,
+        double vfov, // vertical field-of-view in degrees
+        double aspect_ratio) {
+        auto theta = degrees_to_radians(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2.0 * h;
         auto viewport_width = aspect_ratio * viewport_height;
-        auto focal_length = 1.0;
 
-        origin = point3{ 0, 0, 0 };
-        horizontal = vec3{ viewport_width, 0, 0 };
-        vertical = vec3{ 0, viewport_height, 0 };
-        lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3{ 0, 0, focal_length };
+        auto w = unit_vector(look_from - look_at);
+        auto u = unit_vector(cross(vup, w));
+        auto v = cross(w, u);
+
+        origin = look_from;
+        horizontal = viewport_width * u;
+        vertical = viewport_height * v;
+        lower_left_corner = origin - horizontal / 2 - vertical / 2 - w;
     }
 
-    ray get_ray(double u, double v) const {
-        return ray{ origin, lower_left_corner + u * horizontal + v * vertical - origin };
+    ray get_ray(double s, double t) const {
+        return ray{ origin, lower_left_corner + s * horizontal + t * vertical - origin };
     }
 
 private:
--- riow/riow.cpp
+++ riow/riow.cpp
@@ -44,21 +44,21 @@ int main()
 
     // World
 
+    auto R = cos(pi / 4);
     hittable_list world;
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left = make_shared<dielectric>(1.5);
-    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.4, material_left));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), -0.45, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
-    // Camera
-    camera cam;
+    camera cam(point3(-2, 2, 1), point3(0, 0, -1), vec3(0, 1, 0), 20, aspect_ratio);
 
     // Render
 
