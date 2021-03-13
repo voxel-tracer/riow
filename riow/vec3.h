7b507e74470848e7808defc57bf4c1f3ae9ae5c4
VoxelTracer <voxel.tracer@gmail.com>
2021-03-13 06:42:50 -0800

    Focus blur

--- riow/vec3.h
+++ riow/vec3.h
@@ -134,6 +134,14 @@ vec3 random_in_hemisphere(const vec3& normal) {
         return -in_unit_sphere;
 }
 
+vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3{ random_double(-1,1), random_double(-1,1), 0 };
+        if (p.length_squared() >= 1.0) continue;
+        return p;
+    }
+}
+
 vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2 * dot(v, n) * n;
 }
