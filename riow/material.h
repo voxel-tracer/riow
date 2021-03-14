bc730ebad5a94cd364ce743cb1781f515ea5ea83
VoxelTracer <voxel.tracer@gmail.com>
2021-03-14 16:02:04 -0700

    Diffuse light

--- riow/material.h
+++ riow/material.h
@@ -7,6 +7,10 @@ struct hit_record;
 
 class material {
 public:
+    virtual color emitted(double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
+
     virtual bool scatter(const ray& in, const hit_record& rec, color& attenuation, ray& scattered) const = 0;
 };
 
@@ -77,4 +81,21 @@ private:
         r0 = r0 * r0;
         return r0 + (1 - r0) * pow((1 - cosine), 5);
     }
+};
+
+class diffuse_light : public material {
+public:
+    diffuse_light(shared_ptr<texture> a) : emit(a) {}
+    diffuse_light(color c) : emit(make_shared<solid_color>(c)) {}
+
+    virtual bool scatter(const ray& in, const hit_record& rec, color& attenuation, ray& scattered) const override {
+        return false;
+    }
+
+    virtual color emitted(double u, double v, const point3& p) const override {
+        return emit->value(u, v, p);
+    }
+
+public:
+    shared_ptr<texture> emit;
 };
--- riow/riow.cpp
+++ riow/riow.cpp
@@ -13,23 +13,23 @@
 
 using namespace std;
 
-color ray_color(const ray& r, const hittable_list& world, int depth) {
+color ray_color(const ray& r, const color& background, const hittable_list& world, int depth) {
     // If we've exceeded the ray bounce limit, no more lights gathered
     if (depth == 0)
         return color{ 0, 0, 0 };
 
     hit_record rec;
-    if (world.hit(r, 0.001, infinity, rec)) {
-        ray scattered;
-        color attenuation;
-        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered, world, depth - 1);
-        return color{ 0, 0, 0 };
-    }
+    if (!world.hit(r, 0.001, infinity, rec))
+        return background;
 
-    vec3 unit_direction = r.direction();
-    auto t = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - t) * color { 1.0, 1.0, 1.0 } + t * color{ 0.5, 0.7, 1.0 };
+    ray scattered;
+    color attenuation;
+    color emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
+
+    if (!rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+        return emitted;
+
+    return emitted + attenuation * ray_color(scattered, background, world, depth - 1);
 }
 
 hittable_list earth() {
@@ -58,6 +58,31 @@ hittable_list three_spheres() {
     return world;
 }
 
+hittable_list four_spheres() {
+    auto checker = make_shared<checker_texture>(color(0.2, 0.3, 0.1), color(0.9, 0.9, 0.9));
+    auto material_ground = make_shared<lambertian>(checker);
+
+    auto earth_texture = make_shared<image_texture>("earthmap.jpg");
+    auto material_center = make_shared<lambertian>(earth_texture);
+    auto material_right = make_shared<dielectric>(1.5);
+    auto material_left = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    auto material_light = make_shared<diffuse_light>(color(10.0, 10.0, 10.0));
+
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -0.5), 0.5, material_left));
+
+    world.add(make_shared<sphere>(point3(1.0, 0.25, -1.0), 0.5, material_right));
+    world.add(make_shared<sphere>(point3(1.0, 0.25, -1.0), -0.45, material_right));
+
+    world.add(make_shared<sphere>(point3(0.0, 2.0, 0.0), 0.25, material_light));
+
+    return world;
+}
+
 int main()
 {
     // Image
@@ -76,22 +101,33 @@ int main()
     point3 lookat;
     auto vfov = 40.0;
     auto aperture = 0.0;
+    color background = { 0, 0, 0 };
 
     switch (0) {
         case 1:
             world = earth();
+            background = color(0.70, 0.80, 1.00);
             lookfrom = point3(13, 2, 3);
             lookat = point3(0, 0, 0);
             vfov = 20.0;
             break;
 
         case 2:
-        default:
             world = three_spheres();
             lookfrom = point3(3, 2, 2);
             lookat = point3(0, 0, -1);
             vfov = 20.0;
             aperture = 0.1;
+            background = color(0.70, 0.80, 1.00);
+            break;
+        default:
+        case 3:
+            world = four_spheres();
+            lookfrom = point3(3, 2, 2);
+            lookat = point3(0, 0, -1);
+            vfov = 30.0;
+            aperture = 0.1;
+            background = color(0.1, 0.1, 0.1);
             break;
     }
 
@@ -99,7 +135,7 @@ int main()
     vec3 vup{ 0, 1, 0 };
     auto dist_to_focus = (lookfrom - lookat).length();
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
 
     // Render
 
@@ -113,7 +149,7 @@ int main()
                 auto u = (i + random_double()) / (image_width - 1);
                 auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world, max_depth);
+                pixel_color += ray_color(r, background, world, max_depth);
             }
 
             write_color(cout, pixel_color, samples_per_pixel);
